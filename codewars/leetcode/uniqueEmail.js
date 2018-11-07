1b4f4ef46dbdbdeff80474acce0ae51b0108bf00
Ryan Groesch <ryan.groesch@gmail.com>
2018-11-07 12:45:29 -0800

    minor tweak for plus flag

--- codewars/leetcode/uniqueEmail.js
+++ codewars/leetcode/uniqueEmail.js
@@ -41,7 +41,7 @@ var numUniqueEmails = function(emails) {
     console.log(email) 
     // iterate through string looking for special chars if . concat if plus trim until you reach @ symbol.
     for(let i = 0; i < email.length; i++) {
-      if(email[i] === '+') {
+      if(email[i] === '+' && plus === -1) {
         plus = i;
       }
       if (email[i] === '@' && plus !== -1) {
@@ -67,3 +67,6 @@ var numUniqueEmails = function(emails) {
 };
 
 numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]);
+
+let secondAttempt = ["fg.r.u.uzj+o.pw@kziczvh.com","r.cyo.g+d.h+b.ja@tgsg.z.com","fg.r.u.uzj+o.f.d@kziczvh.com","r.cyo.g+ng.r.iq@tgsg.z.com","fg.r.u.uzj+lp.k@kziczvh.com","r.cyo.g+n.h.e+n.g@tgsg.z.com","fg.r.u.uzj+k+p.j@kziczvh.com","fg.r.u.uzj+w.y+b@kziczvh.com","r.cyo.g+x+d.c+f.t@tgsg.z.com","r.cyo.g+x+t.y.l.i@tgsg.z.com","r.cyo.g+brxxi@tgsg.z.com","r.cyo.g+z+dr.k.u@tgsg.z.com","r.cyo.g+d+l.c.n+g@tgsg.z.com","fg.r.u.uzj+vq.o@kziczvh.com","fg.r.u.uzj+uzq@kziczvh.com","fg.r.u.uzj+mvz@kziczvh.com","fg.r.u.uzj+taj@kziczvh.com","fg.r.u.uzj+fek@kziczvh.com"];
+numUniqueEmails(secondAttempt);
