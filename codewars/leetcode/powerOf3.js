2b227354a4fa44646ecd14da15553570dd28c49d
Ryan Groesch <ryan.groesch@gmail.com>
2019-01-14 21:01:03 -0800

    bugs fixed slow solution

--- codewars/leetcode/powerOf3.js
+++ codewars/leetcode/powerOf3.js
@@ -21,10 +21,26 @@ Example 4:
 Input: 45
 Output: false
 */
-
 var isPowerOfThree = function(n) {
-  if(n % 2 === 0) {
+  if(n % 2 === 0 || n < 0) {
+    console.log('moo')
     return false;
   }
-    
+  let i = 0;
+  while(Math.pow(3, i) <= n) {
+    let currentVal = n / Math.pow(3, i);
+    if(n / Math.pow(3, i) === 3 || n === Math.pow(3,i)) {
+      console.log('solved')
+      return true;
+    } else if(currentVal % 3 === 0) {
+      console.log(currentVal)
+      currentVal = currentVal / 3; 
+    } else {
+      console.log('moo2')
+      return false;
+    }
+    i++;
+  }  
 };
+
+isPowerOfThree(45);
